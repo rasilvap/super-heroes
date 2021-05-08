4001282acc43a3c27735d125e19a20f82eb06248
rodolfosilva <rasilvap22@gmail.com>
2021-05-08 12:00:54 -0500

    feat: finalized the initial page design.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import './Home.styles.css'
+import Text from '../../components/atoms/Text'
 
 function Home() {
   return (
     <div>
+      <div className="box-main">
+        <div className="box-text-main">
+          <Text variant="title" color="white">
+            Super heroes
+          </Text>
+        </div>
+
+        <div className="box-text-body">
+          <Text variant="title" color="white">
+            Ver personajes
+          </Text>
+        </div>
+
+        <div className="box-logo-body">
+          <img
+            className="img-logo"
+            alt="logo"
+            src="https://res.cloudinary.com/plac-web/image/upload/v1620492689/marvel/Marvel_Logo.svg.png"
+          />
+        </div>
+      </div>
+
       <img
         alt="Main"
         src="https://res.cloudinary.com/plac-web/image/upload/v1619888975/marvel/main.jpg"
         className="bg-main-image"
       />
-      <Text />
     </div>
   )
 }
